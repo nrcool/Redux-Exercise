b5d5174cc14855f8ab344d305b2c480b2e5779b3
NAQVI <nomanalam7214@gmail.com>
2019-08-27 12:06:52 +0200

    removed console.log

--- src/components/Xcomponent.jsx
+++ src/components/Xcomponent.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { connect } from 'react-redux';
 
 function Xcomponent(props) {
-    console.log(props)
     return (
         <div>
              <h1> Increment X : {props.valuex} <button onClick={props.incrementValueY}>Increment Y</button> </h1>
