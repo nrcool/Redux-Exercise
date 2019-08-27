06eb8ab8b1e295c042fc10b6e89e7fc8b55852fa
NAQVI <nomanalam7214@gmail.com>
2019-08-27 11:33:34 +0200

    created Ycomponent

---
+++ src/components/Ycomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Ycomponent() {
+    return (
+        <div>
+            <h1> Increment Y :  <button>Increment X</button> </h1>
+        </div>
+    )
+}
