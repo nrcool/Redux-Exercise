30654647cdbddae6c91d7e97d15c3b40c778c78c
NAQVI <nomanalam7214@gmail.com>
2019-08-27 11:44:23 +0200

    created redux store

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from "redux";
+import {Provider} from "react-redux";
+import Reducer from "./Reducer/Reducer";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(Reducer);
+
+
+ReactDOM.render(<Provider store ={store}> <App /> </Provider>, document.getElementById('root'));
 
