e8a4d9744c6d197804fd6cf1a1819fbd0b8de43c
NAQVI <nomanalam7214@gmail.com>
2019-08-27 11:58:27 +0200

    connect ed XYcomponents with redux store,and  get data from store

--- src/components/Xcomponent.jsx
+++ src/components/Xcomponent.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { connect } from 'react-redux';
 
-export default function Xcomponent() {
+function Xcomponent(props) {
+    console.log(props)
     return (
         <div>
-             <h1> Increment X :  <button>Increment Y</button> </h1>
+             <h1> Increment X : {props.valuex} <button>Increment Y</button> </h1>
         </div>
     )
 }
+
+const mapStateToProps=(state)=>{
+    return{
+        valuex:state.valuex
+    }
+}
+
+export default connect(mapStateToProps)(Xcomponent)
--- src/components/Ycomponent.jsx
+++ src/components/Ycomponent.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
-export default function Ycomponent() {
+import { connect } from 'react-redux';
+function Ycomponent(props) {
     return (
         <div>
-            <h1> Increment Y :  <button>Increment X</button> </h1>
+            <h1> Increment Y : {props.valuey}  <button>Increment X</button> </h1>
         </div>
     )
 }
+const mapStateToProps=(state)=>{
+    return{
+        valuey:state.valuey
+    }
+}
+export default connect(mapStateToProps)(Ycomponent)
