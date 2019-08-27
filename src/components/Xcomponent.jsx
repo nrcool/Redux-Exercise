768f1078426ffed4184b0cd80703e27c179f1aa7
NAQVI <nomanalam7214@gmail.com>
2019-08-27 11:33:27 +0200

    created xcomponent

---
+++ src/components/Xcomponent.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Xcomponent() {
+    return (
+        <div>
+             <h1> Increment X :  <button>Increment Y</button> </h1>
+        </div>
+    )
+}
