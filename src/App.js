448b7a690efb8c8e1c9c5fda8a80cdec8561f0ee
NAQVI <nomanalam7214@gmail.com>
2019-08-27 11:32:58 +0200

    imported X and Y compoenet

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import "./App.css"
+import "./App.css";
+import Xcomponent from './components/Xcomponent';
+import Ycomponent from './components/Ycomponent';
+
+
+
 function App() {
   return (
     <div className="App">
-   
+      <Xcomponent/>
+      <Ycomponent/>
     </div>
   );
 }
